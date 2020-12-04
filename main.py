f875dfa9b9e867587f35423f78101aa839bebd58
Victorlouisdg <victorlouisdg@gmail.com>
2020-12-04 13:28:58 +0100

    start for adding bones

--- main.py
+++ main.py
@@ -56,11 +56,14 @@ def find_child_joints(joints, link):
     return child_joints
         
         
-def recurse(link, empty):
+def recurse(link, empty, bone):
     child_joints = find_child_joints(joints, link)
     print(child_joints)
     for child_joint in child_joints:
+        bpy.ops.object.select_all(action='DESELECT')
         bpy.context.view_layer.objects.active = empty
+        empty.select_set(True)
+    
         bpy.ops.object.duplicate(linked=False, mode='TRANSLATION')
         new_empty = bpy.context.active_object
         new_empty.name = child_joint.attrib['name']
@@ -76,22 +79,46 @@ def recurse(link, empty):
         new_empty.rotation_euler.rotate_axis("X", roll)
         new_empty.rotation_euler.rotate_axis("Y", pitch)
         new_empty.rotation_euler.rotate_axis("Z", yaw)
+
+        bpy.ops.object.mode_set(mode='OBJECT') # .select only has an effect in object mode (don't ask me why)
         
         
+        armature = bpy.data.objects['Armature']
+        bpy.ops.object.select_all(action='DESELECT')
+        bpy.context.view_layer.objects.active = armature
+        armature.select_set(True)
         
-        # TODO add bones here
+        bpy.ops.object.mode_set(mode='EDIT') # go back to Edit Mode, to see the results
+
+        for b in armature.data.bones: # deselect the other bones
+            b.select = False
+            b.select_tail = False
+            b.select_head = False
+
+        print(bone)
+        bone.select_tail = True
+        
+        bpy.ops.armature.extrude_move(TRANSFORM_OT_translate={"value":new_empty.location.xyz, "orient_matrix":((1, 0, 0), (0, 1, 0), (0, 0, 1)), "orient_matrix_type":'GLOBAL'})
+        bpy.ops.object.mode_set(mode='OBJECT')
+        
+        new_tail= bpy.context.active_object
         
         child_link = child_joint.find('child').attrib['link']
-        recurse(child_link, new_empty)
+        recurse(child_link, new_empty, new_tail)
     
 
 # TODO check spec whether multiple roots can even exist
+
 for root_link in root_links:
     bpy.ops.object.empty_add(type='ARROWS', align='WORLD', location=(0, 0, 0), scale=(1, 1, 1))
     bpy.context.object.empty_display_size = 0.2
     empty = bpy.context.active_object
     empty.name = root_link
-    recurse(root_link, empty)
+    
+    bpy.ops.object.armature_add(radius=1, enter_editmode=False, align='WORLD', location=(0, 0, -1), scale=(1, 1, 1))
+    armature  = bpy.context.active_object
+    tail = armature.data.bones['Bone'].tail
+    recurse(root_link, empty, tail)
         
         
     
