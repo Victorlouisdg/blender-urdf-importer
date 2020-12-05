5abd5f0711fd48f4dbbdb5882ca43cbbe12e5e61
vdgussem <victorlouis.degusseme@ugent.be>
2020-12-05 18:49:55 +0100

    more cleanup

--- main.py
+++ main.py
@@ -35,78 +35,85 @@ rootlinks = find_rootlinks(joints)
 
 def find_childjoints(joints, link):
     """Returns all joints that contain the link as parent"""
-    child_joints = []
+    childjoints = []
     for joint in joints:
         if joint.find('parent').attrib['link'] == link:
             childjoints.append(joint)
-    return childdjoints
+    return childjoints
         
     
 def select_only(blender_object):
     """Selects and actives a Blender object and deselects all others"""
+    print('MODE IS:', bpy.context.mode)
     bpy.ops.object.select_all(action='DESELECT')
     bpy.context.view_layer.objects.active = blender_object
     blender_object.select_set(True)
+    
+    
+def add_next_empty(empty, joint):
+    """Duplicates the empty and applies the transform specified in the joint"""
+    select_only(empty)
+    
+    bpy.ops.object.duplicate()
+    new_empty = bpy.context.active_object
+    new_empty.name = joint.attrib['name']
+    
+    translation = [float(s) for s in joint.find('origin').attrib['xyz'].split()]
+    bpy.ops.transform.translate(value=translation, orient_type='LOCAL')
+    
+    roll, pitch, yaw = [float(s) for s in joint.find('origin').attrib['rpy'].split()]    
+    new_empty.rotation_euler.rotate_axis("X", roll)
+    new_empty.rotation_euler.rotate_axis("Y", pitch)
+    new_empty.rotation_euler.rotate_axis("Z", yaw)
+    return new_empty
+    
         
 def add_childjoints(link, empty, bone):
     childjoints = find_childjoints(joints, link)
-    print(child_joints)
-    for child_joint in child_joints:
-        select_only(empty)
-    
-        bpy.ops.object.duplicate(linked=False, mode='TRANSLATION')
-        new_empty = bpy.context.active_object
-        new_empty.name = childjoint.attrib['name']
+    for childjoint in childjoints:
+        new_empty = add_next_empty(empty, childjoint)
         
-        translation = [float(s) for s in childjoint.find('origin').attrib['xyz'].split()]
-        bpy.ops.transform.translate(value=translation, orient_type='LOCAL')
+        print('MODE IS:', bpy.context.mode)
+       
+        armature_object = bpy.data.objects['Armature']
+        select_only(armature_object)
         
-        roll, pitch, yaw = [float(s) for s in childjoint.find('origin').attrib['rpy'].split()]
-        
-        print('rpy', roll, pitch, yaw)
-        
-        new_empty.rotation_euler.rotate_axis("X", roll)
-        new_empty.rotation_euler.rotate_axis("Y", pitch)
-        new_empty.rotation_euler.rotate_axis("Z", yaw)
+        bpy.ops.object.mode_set(mode='EDIT')
 
-        bpy.ops.object.mode_set(mode='OBJECT') # .select only has an effect in object mode (don't ask me why)
-        
-        
-        armature = bpy.data.objects['Armature']
-        bpy.ops.object.select_all(action='DESELECT')
-        bpy.context.view_layer.objects.active = armature
-        armature.select_set(True)
-        
-        bpy.ops.object.mode_set(mode='EDIT') # go back to Edit Mode, to see the results
-
-        for b in armature.data.bones: # deselect the other bones
+        for b in armature_object.data.edit_bones: # deselect the other bones
             b.select = False
             b.select_tail = False
             b.select_head = False
 
-        print(bone)
         bone.select_tail = True
-        
+                
         bpy.ops.armature.extrude_move(TRANSFORM_OT_translate={"value":new_empty.location.xyz, "orient_matrix":((1, 0, 0), (0, 1, 0), (0, 0, 1)), "orient_matrix_type":'GLOBAL'})
+        
+        new_bone = 
+        
         bpy.ops.object.mode_set(mode='OBJECT')
-        
-        new_tail= bpy.context.active_object
-        
-        child_link = child_joint.find('child').attrib['link']
+#        
+#        new_tail= bpy.context.active_object
+#        
+#        child_link = child_joint.find('child').attrib['link']
         #recurse(child_link, new_empty, new_tail)
     
 
 # TODO check spec whether multiple roots can even exist
+
 for rootlink in rootlinks:
     bpy.ops.object.empty_add(type='ARROWS', align='WORLD', location=(0, 0, 0), scale=(1, 1, 1))
     bpy.context.object.empty_display_size = 0.2
     empty = bpy.context.active_object
-    empty.name = root_link
+    empty.name = rootlink
     
     bpy.ops.object.armature_add(radius=1, enter_editmode=False, align='WORLD', location=(0, 0, -1), scale=(1, 1, 1))
     armature  = bpy.context.active_object
-    bone = armature.data.bones['Bone']
-    recurse(rootlink, empty, bone)  
+    
+    bpy.ops.object.mode_set(mode='EDIT')
+    bone = armature.data.edit_bones['Bone']
+    bpy.ops.object.mode_set(mode='OBJECT')
+    add_childjoints(rootlink, empty, bone)  
 
 
 # TODO 
